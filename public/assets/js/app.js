4dcca4287d3439004045304ed6b8f689102f0003
Mike Erickson <codedungeon@gmail.com>
2013-11-05 12:22:30 -0800

    Refactored app.js code to cache variables

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -1,10 +1,17 @@
 $(function() {
-	$('input[type=submit]').attr('disabled', 'disabled');
+	var mbrFName  = $('#memberFName'),
+		mbrLName  = $('#memberLName'),
+		mbrNo     = $('#memberNo'),
+		mbrErr    = $('.memberError'),
+		btnSubmit = $('input[type=submit]'),
+		baseUrl   = 'http://localhost:8010/members/';
+
+	btnSubmit.attr('disabled', 'disabled');
 
 	// the first and last name field are disabled (you can enable if you want)
 	// figured due to data integrity, you would want this information to be the same
-	$('#memberFName').attr('disabled', 'disabled');
-	$('#memberLName').attr('disabled', 'disabled');
+	//mbrFName.attr('disabled', 'disabled');
+	//mbrLName.attr('disabled', 'disabled');
 
 	// this hides any flash message after 500ms of display
 	$('#flash-message').delay(3000).fadeOut(500);
@@ -13,21 +20,20 @@ $(function() {
 	// .done will be called if a value is found
 	// .fail method will be called if a member record is NOT found
 	// If you are serving on a port other than 8000, you will need to change the URL
-	$('#memberNo').on('blur', function(){
-		var memberNo = $('#memberNo').val(),
-		    url      = 'http://localhost:8000/members/'+memberNo;
-		$.getJSON(url, function() {
-		}).done(function(result) {
-				$('.memberError').hide();
-				$('#memberFName').val(result.first_name);
-				$('#memberLName').val(result.last_name);
-				$('input[type=submit]').removeAttr('disabled');
-		}).fail(function(){
-				$('.memberError').show();
-				$('#memberFName').val('');
-				$('#memberLName').val('');
-				$('#memberNo').focus();
-				$('input[type=submit]').attr('disabled', 'disabled');
+	mbrNo.on('blur', function() {
+		var memberNo = mbrNo.val();
+		$.getJSON(baseUrl+memberNo, function() {
+			}).done(function(result) {
+					mbrErr.hide();
+					mbrFName.val(result.first_name);
+					mbrLName.val(result.last_name);
+					btnSubmit.removeAttr('disabled');
+			}).fail(function(){
+					mbrErr.show();
+					mbrFName.val('');
+					mbrLName.val('');
+					mbrNo.focus();
+					btnSubmit.attr('disabled', 'disabled');
 		});
 	});
 });
